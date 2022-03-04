003030c6169bda7e007183b5824cb1504193e9eb
GameProarmy <63298525+GameProarmy@users.noreply.github.com>
2022-03-04 19:39:41 +0100

    Update and rename plugins/rules.js to Plugins/rules.js

---
+++ Plugins/rules.js
@@ -0,0 +1,28 @@
+let handler = async m => m.reply(`
+   *_OWNER GRUPPEN_*
+  *________*
+   *ɢᴜᴛᴇɴ ᴍᴏʀɢᴇɴ ᴅᴇᴜᴛꜱᴄʜʟᴀɴᴅ*
+   _https://chat.whatsapp.com/LXOCuNPvtjrAlTz7vCahsT_
+   
+   *✰𝕬søչiα𝖑✰*
+   _https://chat.whatsapp.com/BkYpHjIDoLMIbMNKSuJ0Ut_
+   
+   *𝔹𝕒𝕕𝔹𝕠𝕪𝕤⚔𝔅𝔞𝔡𝔊𝔦𝔯𝔩𝔰😈*
+  _https://chat.whatsapp.com/F4DjAcpAiFo80dtbbyU86S_
+  
+  *Nike und adidas*
+  _https://chat.whatsapp.com/IEP0tEpCUo4DyoTrVY838d_
+  
+  *Trustbot cg/Info/FAQ*
+  _https://chat.whatsapp.com/Cvu7evVuWDw9sN3g7v6rjG_
+  *________* 
+Infos:*Wenn es einen Fehler,bug gibt bitte an den owner wenden!*
+
+  _*!!!Sollte wer GEGENWERBUNG machen gibt es einen bot ban+werbe spam!!!*_
+ 
+`.trim()) // Tambah sendiri kalo mau
+handler.help = ['Gruppen']
+handler.tags = ['Gruppen']
+handler.command = /^Gruppen$/i
+
+module.exports = handler
