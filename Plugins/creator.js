c8df97c7ad54a0adbeb7567c42ada1c544b0efa2
GameProarmy <63298525+GameProarmy@users.noreply.github.com>
2022-03-04 19:37:25 +0100

    Update and rename plugins/creator.js to Plugins/creator.js

---
+++ Plugins/creator.js
@@ -0,0 +1,11 @@
+function handler(m) {
+  // Ini cuma contoh, jgn di uncomment -_-
+  // F this.sendContact(m.chat, '4915905890216', '꧁༺「✯ꌛ℮ꁣꁣꊛꈛ✯ī.am 」༻꧂⁩', m)
+  this.sendContact(m.chat, '4915905890216', '꧁༺「✯ꌛ℮ꁣꁣꊛꈛ✯ī.am 」༻꧂⁩ ', m)
+}
+handler.help = ['owner/creator']
+handler.tags = ['info']
+
+handler.command = /^(owner|creator)$/i
+
+module.exports = handler
