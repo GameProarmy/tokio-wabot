7ebb657cec82456a008ba86d93de77602fa64e21
GameProarmy <63298525+GameProarmy@users.noreply.github.com>
2022-03-04 19:35:37 +0100

    Update and rename plugins/add.js to Plugins/add.js

--- Plugins/add.js
+++ Plugins/add.js
@@ -32,7 +32,7 @@ let handler = async (m, { conn, text, participants, usedPrefix, command }) => {
   }
 }
 handler.help = ['add/+'].map(v => v + ' number,number')
-handler.tags = ['admin']
+handler.tags = ['host']
 handler.command = /^(add|\+)$/i
 handler.owner = false
 handler.mods = false
@@ -47,4 +47,3 @@ handler.fail = null
 handler.limit = true
 
 module.exports = handler
-
