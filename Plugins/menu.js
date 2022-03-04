f75545c16af6cbdff96a0a878c0fca4f7ee7123e
GameProarmy <63298525+GameProarmy@users.noreply.github.com>
2022-03-04 19:46:16 +0100

    Update and rename plugins/menu.js to Plugins/menu.js

--- Plugins/menu.js
+++ Plugins/menu.js
@@ -34,24 +34,22 @@ let tags = {
 const defaultMenu = {
   before: `
 
-🙏🏻 Namaste %name, how can i help you?
+Member name: %name,
 
-🪵 Left: *%limit Limit*
-🎗️ Role: *%role*
++ Role: *%role*
 🛕 Level: *%level* 
 ☕ Total XP: *%totalexp*
 
 〽️ Prefix: *%p*
 📅 Date: *%week, %date*
-💠 Github: github.com/itsajaygaur/tokio-wabot
 
-👇🏻 All usable commands are listed below 
+↓ Alle verwendbaren Befehle sind unten aufgelistet↓ 
 
 %readmore`.trimStart(),
   header: '        *━━❰･%category･❱━━*',
-  body: ' 🌠 %cmd %islimit %isPremium',
+  body: ' ➤ %cmd %islimit %isPremium',
   footer: ' ',
-  after: `🌟 *Hope you're enjoying bot, have a great day* 
+  after: `*Bot zurzeit noch in Beta Version!* 
 `,
 }
 let handler = async (m, { conn, usedPrefix: _p }) => {
@@ -150,7 +148,7 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       readmore: readMore
     }
     text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), (_, name) => '' + replace[name])
-    conn.send2ButtonImg(m.chat, thumb, `🏮 I\'m ${conn.user.name}`, text.trim(), 'owner', '-owner', 'rules', '.rules', m)
+    conn.send2ButtonImg(m.chat, thumb, text.trim(), '', '!owner', 'gruppen', '!gruppen', m)
   } catch (e) {
     conn.reply(m.chat, 'Sorry, the menu is in error', m)
     throw e
