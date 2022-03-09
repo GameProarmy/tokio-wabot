82d9c97be3b2c8b75530a77cba401a89813f2a23
GameProarmy <63298525+GameProarmy@users.noreply.github.com>
2022-03-09 11:01:48 +0100

    Update creator.js

--- Plugins/creator.js
+++ Plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
   // F this.sendContact(m.chat, '4915905890216', '꧁༺「✯ꌛ℮ꁣꁣꊛꈛ✯ī.am 」༻꧂⁩', m)
-  this.sendContact(m.chat, '4915905890216', '꧁༺「✯ꌛ℮ꁣꁣꊛꈛ✯ī.am 」༻꧂⁩ ', m)
+  conn.reply(m.chat, `wa.me/+4915905890216`, m)
 }
 handler.help = ['owner/creator']
 handler.tags = ['info']
