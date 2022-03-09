05b729b64cf4af6593973632e942e49c878159c0
GameProarmy <63298525+GameProarmy@users.noreply.github.com>
2022-03-09 10:50:15 +0100

    Update config.js

--- config.js
+++ config.js
@@ -28,8 +28,8 @@ global.APIKeys = { // APIKey Here
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'I hope you\'re fine'
-  var sticker_author = 'kazukafu'
+  var sticker_name = 'Guck net du jockel'
+  var sticker_author = 'Trustbot '
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
