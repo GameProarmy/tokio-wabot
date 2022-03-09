d75c47524bb8a163e8d78b79f3b15c60f3f74f85
GameProarmy <63298525+GameProarmy@users.noreply.github.com>
2022-03-09 10:57:00 +0100

    Update _antilink.js

--- Plugins/_antilink.js
+++ Plugins/_antilink.js
@@ -1,6 +1,6 @@
 let handler = m => m
 
-let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})/i
+let linkRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]/i
 handler.before = async function (m, { user, isBotAdmin, isAdmin }) {
   if ((m.isBaileys && m.fromMe) || m.fromMe || !m.isGroup) return true
   let chat = global.DATABASE.data.chats[m.chat]
